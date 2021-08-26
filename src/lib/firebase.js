27e7a98f5ba76a80a5608f4b2cd566715d01da45
aresous <aresous@gmail.com>
2021-08-26 15:56:10 -0400

    login page functional

---
+++ src/lib/firebase.js
@@ -0,0 +1,18 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyDoZ-9qVvTqt8l-ZYOyPbRHdeCnxQDTEwg",
+    authDomain: "instagram-b2431.firebaseapp.com",
+    projectId: "instagram-b2431",
+    storageBucket: "instagram-b2431.appspot.com",
+    messagingSenderId: "510199318308",
+    appId: "1:510199318308:web:acdbb68752302b9735d5b1"
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+
+export {firebase, FieldValue}
---
+++ src/seed.js
@@ -0,0 +1,74 @@
+
+// NOTE: replace 'NvPY9M9MzFTARQ6M816YAzDJxZ72' with your Firebase auth user id (can be taken from Firebase)
+export function seedDatabase(firebase) {
+    const users = [
+      {
+        userId: 'kzCjyviw5IQqmRWjB1q5cKEmrYN2',
+        username: 'devon',
+        fullName: 'Devon Something',
+        emailAddress: 'aresous@gmail.com',
+        following: ['2'],
+        followers: ['2', '3', '4'],
+        dateCreated: Date.now()
+      },
+      {
+        userId: '2',
+        username: 'raphael',
+        fullName: 'Raffaello Sanzio da Urbino',
+        emailAddress: 'raphael@sanzio.com',
+        following: [],
+        followers: ['kzCjyviw5IQqmRWjB1q5cKEmrYN2'],
+        dateCreated: Date.now()
+      },
+      {
+        userId: '3',
+        username: 'dali',
+        fullName: 'Salvador Dalí',
+        emailAddress: 'salvador@dali.com',
+        following: [],
+        followers: ['kzCjyviw5IQqmRWjB1q5cKEmrYN2'],
+        dateCreated: Date.now()
+      },
+      {
+        userId: '4',
+        username: 'orwell',
+        fullName: 'George Orwell',
+        emailAddress: 'george@orwell.com',
+        following: [],
+        followers: ['kzCjyviw5IQqmRWjB1q5cKEmrYN2'],
+        dateCreated: Date.now()
+      }
+    ];
+  
+    // eslint-disable-next-line prefer-const
+    for (let k = 0; k < users.length; k++) {
+      firebase.firestore().collection('users').add(users[k]);
+    }
+  
+    // eslint-disable-next-line prefer-const
+    for (let i = 1; i <= 5; ++i) {
+      firebase
+        .firestore()
+        .collection('photos')
+        .add({
+          photoId: i,
+          userId: '2',
+          imageSrc: `/images/users/raphael/${i}.jpg`,
+          caption: 'Saint George and the Dragon',
+          likes: [],
+          comments: [
+            {
+              displayName: 'dali',
+              comment: 'Love this place, looks like my animal farm!'
+            },
+            {
+              displayName: 'orwell',
+              comment: 'Would you mind if I used this picture?'
+            }
+          ],
+          userLatitude: '40.7128°',
+          userLongitude: '74.0060°',
+          dateCreated: Date.now()
+        });
+    }
+  }
