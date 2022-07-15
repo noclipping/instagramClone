68da8588ab68ca8061dc3d1b86ed64f23f229b3a
aresous <aresous@gmail.com>
2022-07-14 21:53:49 -0400

    instalogo removal

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,15 +1,15 @@
-import {useEffect} from 'react'
-import Header from '../components/header'
-export default function NotFound(){
-    useEffect(()=>{
-        document.title='Not Found - Instagram'
-    }, [])
-    return(
-        <div className="bg-gray-background">
-            <Header/>
-            <div className="mx-auto max-screen-lg">
-                <p className="text-center text-2xl">Not Found!</p>
-            </div>
-        </div>
-    )
+import { useEffect } from "react";
+import Header from "../components/header";
+export default function NotFound() {
+  useEffect(() => {
+    document.title = "Not Found";
+  }, []);
+  return (
+    <div className="bg-gray-background">
+      <Header />
+      <div className="mx-auto max-screen-lg">
+        <p className="text-center text-2xl">Not Found!</p>
+      </div>
+    </div>
+  );
 }
