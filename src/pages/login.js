9ea47db9c9049f170b2736f75a201a1fdac5f9d7
aresous <aresous@gmail.com>
2022-07-25 19:05:21 -0400

    responsive mobile design

--- src/pages/login.js
+++ src/pages/login.js
@@ -27,15 +27,19 @@ export default function Login() {
   }, []);
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="flex w-3/5">
+      <div className="flex w-0  md:w-3/5">
         <img
           src="images/iphone-with-profile.jpg"
           alt="Iphones with instagram"
+          className="invisible sm:visible"
         ></img>
       </div>
-      <div className="flex flex-col w-2/5">
+      <div className="w-full flex flex-col md:w-2/5">
         <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
-          <h1 className="flex justify-center w-full"></h1>
+          <h1 className="flex justify-center w-full">
+            <p className="text-4xl ">Instaclone</p>
+          </h1>
+          <br />
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
           <form onSubmit={handleLogin} method="POST">
--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -62,18 +62,20 @@ export default function SignUp() {
   }, []);
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="flex w-3/5">
+      <div className="flex w-0 md:w-3/5">
         <img src="images/iphone-with-profile.jpg" alt="Iphones"></img>
       </div>
-      <div className="flex flex-col w-2/5">
+      <div className="w-full flex flex-col md:w-2/5">
         <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
           <h1 className="flex justify-center w-full">
+            <p className="text-4xl">Instaclone</p>
             {/* <img
               src="/images/logo.png"
               alt="instaclone"
               className="mt-2 w-6/12 mb-4"
             ></img> */}
           </h1>
+          <br />
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
           <form onSubmit={handleSignUp} method="POST">
